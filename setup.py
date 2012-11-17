e2d0dbc25867f38648179018c7d520fb1e2edcf6
kraiz <git@kraiz.de>
2012-11-17 17:37:16 +0100

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python
-import os, sys
-from distutils.core import setup
+import sys
 
+from distutils.core import setup
+from distutils.command.install_data import install_data
+
+
+class post_install(install_data):
+    def run(self):
+        install_data.run(self)
+        print "ARGV: " + sys.argv[1]
 
 setup(
     name='unblockme',
@@ -20,5 +27,6 @@ setup(
         ('/etc/init.d', ['unblockme/deploy/unblockme'])
     ],
 
-    install_requires=['Twisted>=11']
+    install_requires=['Twisted>=11'],
+    cmdclass=dict(install=post_install)
 )
