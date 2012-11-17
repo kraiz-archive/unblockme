5282744235337945a672a2e5044fea4d24d5f702
kraiz <git@kraiz.de>
2012-11-17 17:02:56 +0100

    fixed setup py

--- setup.py
+++ setup.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python
-import os
+import os, sys
 from distutils.core import setup
 
 
-if __name__ == '__main__':
-    setup(
-        name='unblockme',
-        version='0.0.2a1',
-        author='Lars Kreisz',
-        author_email='der.kraiz@gmail.com',
+setup(
+    name='unblockme',
+    version='0.0.2a1',
+    author='Lars Kreisz',
+    author_email='der.kraiz@gmail.com',
 
-        packages=['unblockme', 'twisted.plugins'],
-        package_data={
-            'twisted': ['plugins/unblockme_plugin.py'],
-            'unblockme': ['deploy/unblockme.conf',
-                          'deploy/unblockme']
-        },
-        data_files=[
-            ('/etc/', ['unblockme/deploy/unblockme.conf']),
-            ('/etc/init.d', ['unblockme/deploy/unblockme'])
-        ],
+    packages=['unblockme', 'twisted.plugins'],
+    package_data={
+        'twisted': ['plugins/unblockme_plugin.py'],
+        'unblockme': ['deploy/unblockme.conf',
+                      'deploy/unblockme']
+    },
+    data_files=[
+        ('/etc/', ['unblockme/deploy/unblockme.conf']),
+        ('/etc/init.d', ['unblockme/deploy/unblockme'])
+    ],
 
-        install_requires=['Twisted>=11']
-    )
-
-    # clean up
-    os.unlink('MANIFEST')
+    install_requires=['Twisted>=11']
+)
