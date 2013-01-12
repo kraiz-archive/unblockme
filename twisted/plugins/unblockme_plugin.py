5fbba0034a7d6ef9e0f7ef5bb48c494be5419496
kraiz <kraiz@dev.(none)>
2013-01-12 12:30:11 +0100

    cleaned branch for new start based on gevent

--- twisted/plugins/unblockme_plugin.py
+++
@@ -1,8 +0,0 @@
-from twisted.application.service import ServiceMaker
-
-unblockme = ServiceMaker(
-    'unblockme',
-    'unblockme.tap',
-    'service for bypassing geo-blocked websites',
-    'unblockme'
-)
